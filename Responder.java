e31dc3195cf002c792cae31351d4f07fef98a79e
SergioDiez <sdgodam@gmail.com>
2014-03-21 20:16:05 +0100

    Clase Responder modificada para generar respuestas aleatorias previamente definidas mediante la inclusion de un atributo Random y un ArrayList<String> con las respuestas, modificado constructor para inicializarlos asi como generateResponse()para devolver valores aleatorios

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,21 @@
  */
 public class Responder
 {
+    Random aleatorio;
+    ArrayList<String> respuestas;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        respuestas = new ArrayList<>();
+        respuestas.add("That sounds interesting. Tell me more...");
+        respuestas.add("Can you explain me your problem?");
+        respuestas.add("Can you say me your network configuration?");
+        respuestas.add("I need more information...");
+        respuestas.add("What OS has your computer?");
+        respuestas.add("Has you install all the recommended security updates?");
     }
 
     /**
@@ -20,6 +33,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return respuestas.get(aleatorio.nextInt(respuestas.size()));
     }
 }
